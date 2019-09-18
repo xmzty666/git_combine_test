087df1d60b840d4d57e229468bd7a6a35696d46d
xmzty666 <55441010+xmzty666@users.noreply.github.com>
2019-09-18 19:54:56 +0800

    Update Applicaiton.java

--- src/main/java/com/baizhi/Applicaiton.java
+++ src/main/java/com/baizhi/Applicaiton.java
@@ -9,5 +9,7 @@ public class Applicaiton {
     public static void main(String[] args) {
         SpringApplication.run(Applicaiton.class, args);
         System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
+        System.out.println("2222222222222222222");
     }
 }
