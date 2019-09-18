1fdf571a2a71f5fe46348907a910ffc83df938a3
Zty <3075302109@qq.com>
2019-09-18 19:25:38 +0800

    v1.1

--- src/main/java/com/baizhi/Applicaiton.java
+++ src/main/java/com/baizhi/Applicaiton.java
@@ -1,6 +1,12 @@
 package com.baizhi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Applicaiton {
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(Applicaiton.class, args);
+    }
 }
